de4a98581f97c203351e995f61df700f30a310c9
AlexKur161 <kursacha@mail.ru>
2022-07-27 18:15:19 +0300

    new

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <h2>кол-во лайков {{likes}}</h2>
+  <h2>кол-во дизлайков {{dislikes}}</h2>
+ <button @click="likesCounter">лайки</button>
+ <button @click="dislikesCounter">дизлайки</button>
+     
+</template>
+<script>
+export default {
+    data(){
+        return{
+            likes: 0,
+            dislikes: 0
+        }
+    },
+    methods:{
+       likesCounter(){
+        this.likes += 1
+       },
+       dislikesCounter(){
+        this.dislikes += 1
+       }
+    }
+}
+</script>
+<style>
+
+</style>
